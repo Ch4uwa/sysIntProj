c54da3f15209be1304d103af0156eb7616ab7c31
Martin Karlsson <mrtn.karlsson@gmail.com>
2019-05-14 14:12:54 +0200

    sync

--- requestAPI.py
+++ requestAPI.py
@@ -4,17 +4,19 @@ import requests
 
 
 class MyRequests:
+    # dict holding the api keys
     APIkeys = {"siteIdAPIkey": "",
                "realTimeAPIkey": "", "weatherApiKey": ""}
 
     def randNames(self):
+        # fetching random names, limit=numbers of names
         data = requests.get('http://api.namnapi.se/v2/names.json?limit=1')
         d = data.json()
         return d
 
-# Show time of departure SL
 
     def slRealTime(self):
+        # Get the siteId of search word
         searchWord = "Gösta Frohms väg(Botkyrka)"
         slStationsOnly = "True"
         slMaxResults = "1"
@@ -26,12 +28,13 @@ class MyRequests:
 
         # get siteid from slSearchSiteID respons
         # set the timewindow to look within
+        # Get departures of buses
         timeWindowMin = "60"
-        # set the args together
         realTimeURL = ("http://api.sl.se/api2/realtimedeparturesV4.json?key=" +
                        self.APIkeys["realTimeAPIkey"] + "&siteid=" + siteIdResp + "&timewindow=" + timeWindowMin)
         slReq = requests.get(realTimeURL)
         try:
+            # if statement might not be needed
             if not slReq.json()["ResponseData"].get("Buses")[0]:
                 return False
             else:
@@ -56,13 +59,14 @@ class MyRequests:
         weather = r.json()
         return weather
 
-    # show local time and maybe choose others
     def worldclockapi(self):
+        # show local time and maybe choose others
         clockUrl = "http://worldclockapi.com/api/json/cet/now?callback=mycallback"
         r = requests.get(clockUrl)
         return r.json()
 
     def numbers(self):
+        # get history from random year
         randNumURL = "http://numbersapi.com/random/year?json"
         r = requests.get(randNumURL)
         return r.json()
